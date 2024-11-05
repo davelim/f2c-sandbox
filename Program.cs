64a5f8dc4c5823b016e9303ddfec12d5e9781781
Dave Lim <dave.lim@ekonbenefits.com>
2024-11-05 13:07:24 -0600

    WebClient (.NETFramework) vs HttpClient (.NET Core)
    Minimal code to GET image and access "Content-Type" from [content]
    headers.

--- Program.cs
+++ Program.cs
@@ -13,16 +13,16 @@ namespace Test.Magick;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async System.Threading.Tasks.Task Main(string[] args)
     {
         { // ImageMagick
             using var image = new ImageMagick.MagickImage(
-                System.IO.Path.GetFullPath("./images/monkey.jpg"));
+                System.IO.Path.GetFullPath("./test/monkey.jpg"));
             using var watermark = new MagickImage(
-                System.IO.Path.GetFullPath("./images/StatementTestOverlay.png"));
+                System.IO.Path.GetFullPath("./test/StatementTestOverlay.png"));
             image.Composite(watermark, Gravity.Center, CompositeOperator.Over);
             image.Write(
-                System.IO.Path.GetFullPath("./images/test_monkey.jpg"));
+                System.IO.Path.GetFullPath("./test/test_monkey.jpg"));
         }
 
 
@@ -69,6 +69,40 @@ public class Program
             Console.WriteLine("core...");
             strm.Seek(0, System.IO.SeekOrigin.Begin);
             printByteArray(cMD5.ComputeHash(strm));
+        }
+
+        {// .NETFramework WebClient to ASP.NET Core HttpClient
+            async System.Threading.Tasks.Task clientTest() {
+                string url = "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU";
+
+#if (NET6_0_OR_GREATER || NETSTANDARD2_0_OR_GREATER)
+                System.Net.Http.HttpClient client = new ();
+                try {
+                    using System.Net.Http.HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Get data and type.
+                    byte[] data = await response.Content.ReadAsByteArrayAsync();
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType = response.Content.Headers.ContentType;
+
+                    Console.WriteLine($"c. content type: {contentType}"); // implicit .ToString()
+                    System.IO.File.WriteAllBytes("./test/c.image.jpg", data);
+                } catch (System.Net.Http.HttpRequestException e) {
+                    Console.WriteLine($"Message: {e.Message}");
+                }
+#else
+                System.Net.WebClient client = new ();
+
+                // Get data and type.
+                byte[] data = client.DownloadData(url);
+                System.Net.WebHeaderCollection responseHeaders = client.ResponseHeaders;
+                string contentType = responseHeaders["Content-Type"];
+
+                Console.WriteLine($"f. content type: {contentType}");
+                System.IO.File.WriteAllBytes("./test/f.image.jpg", data);
+#endif
+            }
+
+            await clientTest();
         }
     }
 }
